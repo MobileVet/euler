14e922a539fe9353b90d7c70b1eeb26f8683f899
Rob Witman <rob@splitmo.com>
2018-12-24 23:28:29 -0500

    Fixed sumOfMultiplesLessThanOrEqualTo tests to reflect algo result
    The constant time algorithm is actually LTE, where it used to be LT.  As such, I
    needed to adjust the test cases to reflect that.

--- src/test/math.test.js
+++ src/test/math.test.js
@@ -1,15 +1,15 @@
 const sut = require('../math')
 
-describe('sumOfMultiplesLessThan', () => {
+describe('sumOfMultiplesLessThanOrEqualTo', () => {
   it('returns 0 when factor is greater than max', () => {
-    expect(sut.sumOfMultiplesLessThan(3, 2)).toEqual(0)
+    expect(sut.sumOfMultiplesLTE(3, 2)).toEqual(0)
   })
 
-  it('returns factor when 2*factor >= max', () => {
-    expect(sut.sumOfMultiplesLessThan(3, 6)).toEqual(3)
+  it('returns factor when 2*factor > max', () => {
+    expect(sut.sumOfMultiplesLTE(3, 5)).toEqual(3)
   })
 
-  it('returns 3*factor when 3*factor >= max', () => {
-    expect(sut.sumOfMultiplesLessThan(3, 9)).toEqual(9)
+  it('returns 3*factor when 2*factor <= max', () => {
+    expect(sut.sumOfMultiplesLTE(3, 6)).toEqual(9)
   })
 })
