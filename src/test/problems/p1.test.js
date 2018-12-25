0d0d7569fdf17f394c871c39b967f84b37833c3f
Rob Witman <rob@splitmo.com>
2018-12-24 23:29:53 -0500

    Collapsed P1 test cases into forEach around all methods tried
    I have been trying different solutions to the problem and was copying in the
    same test cases.  This was tedious and messy, so I extracted out the tests and
    expected values so that I could write the setup once.
    
    Now adding a new method is as easy as adding it to the array of 'solutions'
    
    Similarly, if we want to try another expected result, it can be added to the
    'answers' array.
    
    By adding a solution or answer, ALL methods found in 'solutions' will be tested

--- src/test/problems/p1.test.js
+++ src/test/problems/p1.test.js
@@ -1,81 +1,30 @@
 const sut = require('../../problems/p1')
 
 describe('Problem 1', () => {
-  describe('Counting Algorithm', () => {
-    it('returns 23 when max is 10', () => {
-      // 3 + 5 + 6 + 9 = 23
-      expect(sut.sumOfMultiplesLessThan_count(10)).toEqual(23)
-    })
+  // Solutions listed with fastest first
+  // Top solution time is constant, regardless of max number size
+  const solutions = [['Sum Formula Algorithm', sut.carl],
+                     ['Pattern Formula Algorithm', sut.formula],
+                     ['Slow Counting Algorithm', sut.count],
+                     ['Pattern of Factor List Algorithm', sut.pattern],
+                     ['Modulous Algorithm', sut.mod]],
+        answers = [[10, 23],
+                   [16, 60],
+                   [37, 329],
+                   [100, 2318],
+                   [1000, 233168]]
 
-    it('returns 60 when max is 16', () => {
-      // 3 + 5 + 6 + 9 + 10 + 12 + 15 = 60
-      expect(sut.sumOfMultiplesLessThan_count(16)).toEqual(60)
-    })
+  solutions.forEach(solution => {
+    const [label, fcn] = solution
+    describe(label, () => {
 
-    it('returns 329 when max is 37', () => {
-      expect(sut.sumOfMultiplesLessThan_count(37)).toEqual(329)
-    })
-
-    it('returns 233168 when max is 1000', () => {
-      expect(sut.sumOfMultiplesLessThan_count(1000)).toEqual(233168)
-    })
-  })
-
-  describe('Mod Algorithm', () => {
-    it('returns 23 when max is 10', () => {
-      // 3 + 5 + 6 + 9 = 23
-      expect(sut.sumOfMultiplesLessThan_mod(10)).toEqual(23)
-    })
-
-    it('returns 60 when max is 16', () => {
-      // 3 + 5 + 6 + 9 + 10 + 12 + 15 = 60
-      expect(sut.sumOfMultiplesLessThan_mod(16)).toEqual(60)
-    })
-
-    it('returns 233168 when max is 1000', () => {
-      expect(sut.sumOfMultiplesLessThan_mod(1000)).toEqual(233168)
+      answers.forEach(answer => {
+        const [max, result] = answer
+        it(`returns ${result} when max = ${max}`, () => {
+          expect(fcn(3, 5, max)).toEqual(result)
+        })
+      })
     })
   })
 
-  describe('Pattern Algorithm', () => {
-    it('returns 23 when max is 10', () => {
-      // 3 + 5 + 6 + 9 = 23
-      expect(sut.sumOfMultiplesLessThan_pattern(10)).toEqual(23)
-    })
-
-    it('returns 60 when max is 16', () => {
-      // 3 + 5 + 6 + 9 + 10 + 12 + 15 = 60
-      expect(sut.sumOfMultiplesLessThan_pattern(16)).toEqual(60)
-    })
-
-    it('returns 233168 when max is 1000', () => {
-      expect(sut.sumOfMultiplesLessThan_pattern(1000)).toEqual(233168)
-    })
-
-  })
-
-  describe('Formula Algorithm', () => {
-    it('returns 23 when max is 10', () => {
-      // 3 + 5 + 6 + 9 = 23
-      expect(sut.sumOfMultiplesLessThan_formula(10)).toEqual(23)
-    })
-
-    it('returns 60 when max is 16', () => {
-      // 3 + 5 + 6 + 9 + 10 + 12 + 15 = 60
-      expect(sut.sumOfMultiplesLessThan_formula(16)).toEqual(60)
-    })
-
-    it('returns 329 when max is 37', () => {
-      expect(sut.sumOfMultiplesLessThan_formula(37)).toEqual(329)
-    })
-
-    it('returns 2318 when max is 100', () => {
-      expect(sut.sumOfMultiplesLessThan_formula(100)).toEqual(2318)
-    })
-
-    it('returns 233168 when max is 1000', () => {
-      expect(sut.sumOfMultiplesLessThan_formula(1000)).toEqual(233168)
-    })
-
-  })
 })
