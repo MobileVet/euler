bf41720af9517444b7b855ef894d259ab80e78d8
Rob Witman <rob@splitmo.com>
2018-12-24 23:24:29 -0500

    Moved solutions to P1 file, Math now holds single fcn - TESTS FAIL
    Had originally expected to reuse the sumOfMultiplesLessThan function and thus it
    was placed in the 'math' file.  However, I was also experimenting with various
    solutions and that started to clog the 'math' file with suboptimal
    implementations.
    
    Pulled all suboptimal solutions out to the P1 file.  Added the constant time
    solution helper function to 'math'

--- src/math.js
+++ src/math.js
@@ -1,117 +1,12 @@
-// local helpers
 
-// Calculate the sum of an array of repeated values
-// The offset is based on what 'row' of the repeat we are on
-// NOTE: only sum if less than our max value
-const partialArraySum = (array, offset, max) => {
-  let length = array.length,
-      sum = 0
-  for (let y = 0; y < length; y++) {
-    const value = offset + array[y]
-    sum += (value < max) ? value : 0
-  }
-  return sum
-}
-
-// Produce an array of numbers that are factors of 2 numbers
-// NOTE: These are repeated for every n after n * f1 * f2
-const repeatedTerms = (f1, f2) => {
-  let array = []
-  const greatest = (f1 < f2) ? f2 : f1,
-        product = f1 * f2
-
-  for (let x = 1; x < greatest; x++) {
-    if (f1 * x < product) {
-      array.push(f1 * x)
-    }
-    if (f2 * x < product) {
-      array.push(f2 * x)
-    }
-  }
-  array.push(product)
-  return array
-}
-
-// different algorithms
-
-// Simply count by factor and sum the results until max
-const sumOfMultiplesLessThan_count = (factor, max) => {
-  let currentValue = factor,
-      sum = 0
-  while (currentValue < max) {
-    sum += currentValue
-    currentValue += factor
-  }
-  return sum
-}
-
-// Step from 1 to max,
-// If value is % of f1 or f2, add it to our accumulator
-const sumOfMultiplesLessThan_mod = (f1, f2, max) => {
-  let sum = 0
-  for (let x = 0; x < max; x++) {
-    // Could use ||, but less optimal
-    sum += (x % f1 === 0)
-      ? x
-      : (x % f2 === 0)
-      ? x
-      : 0
-  }
-  return sum
-}
-
-const sumOfMultiplesLessThan_pattern = (f1, f2, max) => {
-  const product = f1 * f2,
-        wholeFactor = ~~(max / product) // ~~ => floor
-
-  let factors = repeatedTerms(f1, f2),
-      sum = 0
-
-  // Sum up the values by looping through our repeated
-  // terms and multiplying our whole divisor
-  const factorLength = factors.length
-  for (let x = 0; x <= wholeFactor; x++) {
-    for (let y = 0; y < factors.length; y++) {
-      const value = product * x + factors[y]
-      sum += (value < max) ? value : 0
-    }
-  }
-  return sum
-}
-
-
-const sumOfMultiplesLessThan_formula = (f1, f2, max) => {
-  const sumFcn = (max) => {
-          let sum = 0
-          for (let x = 0; x < max; x++) {
-            sum += x
-          }
-          return sum
-        },
-        sumArray = (array) => {
-          const length = array.length
-          let sum = 0
-          for (let x = 0; x < length; x++) {
-            sum += array[x]
-          }
-          return sum
-        }
-
-  const array = repeatedTerms(f1, f2),
-        arraySum = sumArray(array),
-        product = f1 * f2,
-        wholeFactor = ~~ (max / product),
-        wfSum = sumFcn(wholeFactor),
-        wholeSum = array.length * wfSum * product + (arraySum * wholeFactor),
-        partialSum = partialArraySum(array, product * wholeFactor, max)
-
-  return wholeSum + partialSum
+// Modification of the Carl Friedrich Gauss equation for calculation
+// of sum of the first n natural numbers.  Adjusted to only return
+// the sum of the those above numbers that are factors of 'multiple'
+const sumOfMultiplesLTE = (multiple, max) => {
+  const m = ~~ (max / multiple)
+  return multiple * (m * (m + 1) / 2)
 }
 
 module.exports = {
-  sumOfMultiplesLessThan: sumOfMultiplesLessThan_formula,
-  sumOfMultiplesLessThan_count,
-  sumOfMultiplesLessThan_formula,
-  sumOfMultiplesLessThan_mod,
-  sumOfMultiplesLessThan_pattern
+  sumOfMultiplesLTE
 }
--- src/problems/p1.js
+++ src/problems/p1.js
@@ -1,25 +1,142 @@
 const M = require('../math')
 
-const sumOfMultiplesLessThan_count = (max) => {
-  const sumOfThree = M.sumOfMultiplesLessThan_count(3, max),
-        sumOfFive = M.sumOfMultiplesLessThan_count(5, max),
-        sumOfFifteen = M.sumOfMultiplesLessThan_count(15, max)
+// local helpers
+
+// Calculate the sum of an array of repeated values
+// The offset is based on what 'row' of the repeat we are on
+// NOTE: only sum if less than our max value
+const partialArraySum = (array, offset, max) => {
+  let length = array.length,
+      sum = 0
+  for (let y = 0; y < length; y++) {
+    const value = offset + array[y]
+    sum += (value < max) ? value : 0
+  }
+  return sum
+}
+
+// Produce an array of numbers that are factors of 2 numbers
+// NOTE: These are repeated for every n after n * f1 * f2
+const repeatedTerms = (f1, f2) => {
+  let array = []
+  const greatest = (f1 < f2) ? f2 : f1,
+        product = f1 * f2
+
+  for (let x = 1; x < greatest; x++) {
+    if (f1 * x < product) {
+      array.push(f1 * x)
+    }
+    if (f2 * x < product) {
+      array.push(f2 * x)
+    }
+  }
+  array.push(product)
+  return array
+}
+
+// different algorithms
+
+// Simply count by factor and sum the results until max
+const sumOfMultiplesLessThan_count = (factor, max) => {
+  let currentValue = factor,
+      sum = 0
+  while (currentValue < max) {
+    sum += currentValue
+    currentValue += factor
+  }
+  return sum
+}
+
+// Step from 1 to max,
+// If value is % of f1 or f2, add it to our accumulator
+const sumOfMultiplesLessThan_mod = (f1, f2, max) => {
+  let sum = 0
+  for (let x = 0; x < max; x++) {
+    // Could use ||, but less optimal
+    sum += (x % f1 === 0)
+      ? x
+      : (x % f2 === 0)
+      ? x
+      : 0
+  }
+  return sum
+}
+
+const sumOfMultiplesLessThan_pattern = (f1, f2, max) => {
+  const product = f1 * f2,
+        wholeFactor = ~~(max / product) // ~~ => floor
+
+  let factors = repeatedTerms(f1, f2),
+      sum = 0
+
+  // Sum up the values by looping through our repeated
+  // terms and multiplying our whole divisor
+  const factorLength = factors.length
+  for (let x = 0; x <= wholeFactor; x++) {
+    for (let y = 0; y < factors.length; y++) {
+      const value = product * x + factors[y]
+      sum += (value < max) ? value : 0
+    }
+  }
+  return sum
+}
+
+
+const sumOfMultiplesLessThan_formula = (f1, f2, max) => {
+  const sumFcn = (max) => {
+          let sum = 0
+          for (let x = 0; x < max; x++) {
+            sum += x
+          }
+          return sum
+        },
+        sumArray = (array) => {
+          const length = array.length
+          let sum = 0
+          for (let x = 0; x < length; x++) {
+            sum += array[x]
+          }
+          return sum
+        }
+
+  const array = repeatedTerms(f1, f2),
+        arraySum = sumArray(array),
+        product = f1 * f2,
+        wholeFactor = ~~ (max / product),
+        wfSum = sumFcn(wholeFactor),
+        wholeSum = array.length * wfSum * product + (arraySum * wholeFactor),
+        partialSum = partialArraySum(array, product * wholeFactor, max)
+
+  return wholeSum + partialSum
+}
+
+// Utilize the modified CFG formula for computing the sum
+// of divisors up to (and including) a max value in addition
+// to our knowledge the idea that the product of the 2 factors
+// is double counted if we look at the sum for each factor individually
+const carl = (f1, f2, max) => {
+  const sumOfThree = M.sumOfMultiplesLTE(f1, max - 1),
+        sumOfFive = M.sumOfMultiplesLTE(f2, max - 1),
+        sumOfFifteen = M.sumOfMultiplesLTE(f1 * f2, max - 1)
 
   return sumOfThree + sumOfFive - sumOfFifteen
 }
 
-const sumOfMultiplesLessThan_formula = (max) =>
-      M.sumOfMultiplesLessThan_formula(3, 5, max)
+// Brute force increment by divisor to generate the sum of numbers
+// less than a max number
+// Recognize that summing two factors will double count their product
+const count = (f1, f2, max) => {
+  const sumOfThree = sumOfMultiplesLessThan_count(f1, max),
+        sumOfFive = sumOfMultiplesLessThan_count(f2, max),
+        sumOfFifteen = sumOfMultiplesLessThan_count(f1 * f2, max)
 
-const sumOfMultiplesLessThan_mod = (max) =>
-      M.sumOfMultiplesLessThan_mod(3, 5, max)
-
-const sumOfMultiplesLessThan_pattern = (max) =>
-      M.sumOfMultiplesLessThan_pattern(3, 5, max)
+  return sumOfThree + sumOfFive - sumOfFifteen
+}
 
 module.exports = {
-  sumOfMultiplesLessThan_count,
-  sumOfMultiplesLessThan_formula,
-  sumOfMultiplesLessThan_mod,
-  sumOfMultiplesLessThan_pattern
+  carl,
+  count,
+  formula: sumOfMultiplesLessThan_formula,
+  mod: sumOfMultiplesLessThan_mod,
+  pattern: sumOfMultiplesLessThan_pattern
 }
